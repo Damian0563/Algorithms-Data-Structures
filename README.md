16f4ccb87f7c3d8fcf02356f83470fb8eac46b86
Damian0563 <damian.realmadrid2005@gmail.com>
2025-08-08 07:14:56 +0200

    queue

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ The following repository houses both implementations and exercises within the re
 The following Data Structures are implemented:
 - Vectors
 - Singly linked lists
+- Queues
 - Doubly linked lists (To be implemenetd)
-- Queues (To be implemenetd)
 - Stacks (To be implemenetd)
 - Rings (To be implemenetd)
 - Binary Trees (To be implemenetd)
---
+++ queues/queue.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <queue>
+template <typename T>
+struct Node{
+    T data;
+    Node* next;
+    Node(const T& data,Node<T>* next=nullptr):data(data),next(next){}
+};
+
+template <typename T>
+class Queue{
+    private:
+        Node<T>* head;
+        Node<T>* tail;
+        unsigned int current_size;
+    public:
+        Queue():head(nullptr), tail(nullptr), current_size(0) {};
+        unsigned int size()const {return this->current_size;}
+        T front() const{return this->head?this->head->data;throw std::out_of_range("no front element in empty queue.");}
+        T back() const{return this->tail?this->tail->data;throw std::out_of_range("no back element in empty queue.");}
+        T pop(){
+            if(!this->isEmpty()){
+                Node<T>* first=this->head;
+                T value=first->data;
+                if(this->head->next) this->head=this->head->next;
+                else{
+                    this->head=nullptr;
+                    this->tail=nullptr;
+                }
+                delete first;
+                this->current_size--;
+                return value;
+            }
+            throw std::out_of_range("pop from empty list");
+        }
+        void push_back(const T& data){
+            Node<T>* new_node=new Node<T>{data,nullptr};
+            if(!this->head){
+                this->head=new_node;
+                this->tail=new_node;
+            }else{
+                this->tail->next=new_node;
+                this->tail=new_node;
+            }
+            this->current_size++;
+        }
+        bool isEmpty() const noexcept{return this->size()==0;}   
+        friend std::ostream& operator<<(std::ostream& os,const Queue<T>& queue){
+            os<<"[";
+            unsigned int size=queue.size();
+            Node<T>* curr=queue.head;
+            while(size>0){
+                os<<curr->data;
+                if(size!=1) os<<",";
+                curr=curr->next;
+                size--;
+            }
+            os<<"]";
+            return os;
+        }
+};
+
+int main(){
+    Queue<int> q;
+    std::cout<<q<<"\n";
+    q.push_back(10);
+    q.push_back(99);
+    q.push_back(21);
+    std::cout<<q<<"\n";
+    int popped=q.pop();
+    std::cout<<"Popped: "<<popped<<"\t"<<"from "<<q<<"\n"; 
+
+    return 1;
+}
