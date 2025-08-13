245b612e673fb58f73d66e6b460ea0a02e11263a
Damian0563 <damian.realmadrid2005@gmail.com>
2025-08-13 06:03:31 +0200

    queues

--- queues/queue.cpp
+++ queues/queue.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 template <typename T>
 struct Node{
     T data;
@@ -128,21 +129,21 @@ struct NodeP{
     T data;
     unsigned int priority;
     NodeP* next;
-    NodeP(const T data,const unsigned int priority,const NodeP<T>* next=nullptr):data(data),priority(priority),next(next){}
+    NodeP(const T data,const unsigned int priority,NodeP<T>* next=nullptr):data(data),priority(priority),next(next){}
 };
 template <typename T>
 class PriorityQueue{
     private:
         NodeP<T>* head;
         NodeP<T>* tail;
-        int size;
+        unsigned int size;
     public:
         PriorityQueue():head(nullptr),tail(nullptr),size(0){};
         PriorityQueue(const PriorityQueue<T>& q){
             NodeP<T>* temp=q.head;
             this->head=nullptr;
             this->tail=nullptr;
-            this->size=q.size();
+            this->size=q.len();
             while(temp){
                 NodeP<T>* new_node=new NodeP<T>{temp->data,temp->priority,nullptr};
                 if(!this->head){
@@ -155,11 +156,24 @@ class PriorityQueue{
                 temp=temp->next;
             }
         }
-        PriorityQueue(const PriorityQueue<T>&& q){
-
+        PriorityQueue(PriorityQueue<T>&& q) noexcept{
+            this->size=q.len();
+            this->head=q.head;
+            this->tail=q.tail;
+            q.head=nullptr;
+            q.tail=nullptr;
+            q.size=0;
         }
         PriorityQueue& operator=(PriorityQueue<T>&& q){
-
+            if(*this==&q) return this;
+            this->clear();
+            this->current_size=q.len();
+            this->head=q.head;
+            this->tail=q.tail;
+            q.head=nullptr;
+            q.tail=nullptr;
+            q.size=0;
+            return this;
         }
         friend std::ostream& operator<<(std::ostream& os,const PriorityQueue<T>& q){
             os<<"[";
@@ -175,6 +189,8 @@ class PriorityQueue{
             return os;
         }
         ~PriorityQueue(){this->clear();}
+        T front() const{return this->head?this->head->data:throw std::out_of_range("no front element in empty queue.");}
+        T back() const{return this->tail?this->tail->data:throw std::out_of_range("no back element in empty queue.");}
         void clear(){
             NodeP<T>* curr=this->head;
             NodeP<T>* temp=nullptr;
@@ -187,22 +203,40 @@ class PriorityQueue{
             this->head=nullptr;
             this->tail=nullptr;
         }
-        inline unsigned int len(){return this->size;}
-        void push_back(T data,int priority){
-            NodeP<T> new_node=new NodeP<T>{data,priority,nullptr};
-            if (!this->head || priority>this->head->priority) {
+        inline unsigned int len() const{return this->size;}
+        void push_back(T data,unsigned int priority=1){
+            NodeP<T>* new_node=new NodeP<T>{data,priority,nullptr};
+            this->size++;
+            if(!this->head || priority>this->head->priority) {
                 new_node->next=this->head;
                 this->head=new_node;
                 if(!this->tail) this->tail=new_node;
             }else{
                 NodeP<T>* curr=this->head;
-                while(curr->next && curr->next->priority>priority){
+                while(curr->next && curr->next->priority>=priority){
                     curr=curr->next;
                 }
                 new_node->next=curr->next;
                 curr->next=new_node;
                 if(!new_node->next) this->tail=new_node;
             }
+        }
+        T pop(){
+            if(!this->head){
+                throw std::out_of_range("can't pop from empty queue.");
+            }
+            T value=this->head->data;
+            NodeP<T>* curr=this->head;
+            if(this->head->next){
+                this->head=this->head->next;
+                this->size--;
+            }else{
+                this->head=nullptr;
+                this->tail=nullptr;
+                this->size=0;
+            }
+            delete curr;
+            return value;
         }
 };
 
@@ -220,9 +254,26 @@ int main(){
     std::cout<<"Popped: "<<popped<<"\t"<<"from "<<q<<"\n";
     std::cout<<q.reverse()<<"\n";
     Queue<int> cpy(q);
+    q.push_back(77);
     std::cout<<cpy<<"\n";
     Queue<int> mo=std::move(q);
     std::cout<<mo<<" "<<q<<"\n";
-
+    std::cout<<"---------------------"<<std::endl;
+    PriorityQueue<int> pq;
+    pq.push_back(21,2);
+    pq.push_back(88);
+    pq.push_back(1,3);
+    pq.push_back(10,2);
+    std::cout<<pq<<"\n";
+    pq.pop();
+    std::cout<<pq<<"\n";
+    PriorityQueue<int> cpq(pq);
+    pq.push_back(44,4);
+    std::cout<<pq<<"\n";
+    std::cout<<cpq<<"\n";
+    PriorityQueue<int> mov=std::move(pq);
+    std::cout<<mov<<" "<<pq<<"\n";
+    PriorityQueue<int> moved(std::move(mov));
+    std::cout<<moved<<" "<<mov<<"\n";
     return 1;
 }
