b6f5f331d4196d3c67c19d0aed1dfc0e0601f750
Damian0563 <damian.realmadrid2005@gmail.com>
2025-09-17 07:29:58 +0200

    graph

---
+++ graphs/graph.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+
+template <typename T>
+struct Vertex{
+    T data;
+    std::vector<Vertex<T>*> edges;
+    Vertex(const T& data):data(data){}
+};
+
+template <typename T>
+class Graph{
+    private:
+        Vertex<T>* head;
+    public:
+        Graph():head(nullptr){}
+        Graph(const T data){
+            Vertex<T>* new_node=new Vertex<T>{data};
+            this->head=new_node;
+        }
+        Graph(const Graph<T>& other){}
+        Graph(Graph<T>&& other){}
+        Graph& operator=(const Graph<T>& other){}
+        friend std::ostream& operator<<(std::ostream& os,const Graph<T>& graph){
+            if (!graph.head) return os;
+            std::queue<Vertex<T>*> q;
+            std::map<T, bool> visited;
+            q.push(graph.head);
+            visited[graph.head->data] = true;
+            while (!q.empty()) {
+                Vertex<T>* node=q.front();
+                q.pop();
+                os<<node->data<<": ";
+                for(auto& vptr : node->edges){
+                    os<<vptr->data<<" ";
+                    if(!visited[vptr->data]){
+                        visited[vptr->data]=true;
+                        q.push(vptr);
+                    }
+                }
+                os<<"\n";
+            }
+            return os;
+        }
+        ~Graph(){this->clear();}
+        void clear(){}
+        void insert(const T element,Vertex<T>* vertex){   
+            if(!vertex) return;
+            if(!this->search(element)){
+                Vertex<T>* new_node=new Vertex<T>{element};
+                vertex->edges.push_back(new_node);
+            }
+        }
+        inline Vertex<T>* get_head()const noexcept{return this->head;}
+        Vertex<T>* search(T data)const noexcept{
+            if(!this->head) return nullptr;
+            std::queue<Vertex<T>*> q;
+            q.push(this->head);
+            while(!q.empty()){
+                Vertex<T>* node=q.front();
+                if (node->data == data) return node;
+                for(auto&itr : node->edges){
+                    q.push(itr);
+                }
+                q.pop();
+            }
+            return nullptr;
+        }
+};
+
+
+int main(){
+    Graph<std::string> flights("Chicago");
+    flights.insert("Washington",flights.get_head());
+    flights.insert("New York",flights.search("Washington"));
+    flights.insert("Denver",flights.search("New York"));
+    flights.insert("Utah",flights.search("Chicago"));
+    flights.insert("Los Angeles",flights.search("Chicago"));
+    std::cout<<flights<<"\n";   
+
+
+    return 0;
+}
